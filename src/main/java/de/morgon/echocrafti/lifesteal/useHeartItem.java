eb6bd6eb71a5d0063c53f13b0f1946f4887b6375
CrAzyA22 <klemm.anton@outlook.de>
2023-08-23 10:26:44 +0200

    there we go

--- src/main/java/de/morgon/echocrafti/lifesteal/useHeartItem.java
+++ src/main/java/de/morgon/echocrafti/lifesteal/useHeartItem.java
@@ -15,14 +15,16 @@ public class useHeartItem implements Listener {
 
         Player p = e.getPlayer();
 
-        if((e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) && p.getInventory().getItemInMainHand().equals("§dHeart")) {
-            p.setMaxHealth(p.getMaxHealth() + 2);
+        if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
 
-            ItemStack h = p.getItemInHand();
-            h.setAmount(h.getAmount() - 1);
+            if(p.getInventory().getItemInMainHand().equals("§dHeart")) {
+                p.setMaxHealth(p.getMaxHealth() + 2);
 
-            p.getInventory().setItemInMainHand(h);
+                ItemStack h = p.getItemInHand();
+                h.setAmount(h.getAmount() - 1);
+
+                p.getInventory().setItemInMainHand(h);
+            }
         }
-
     }
 }
