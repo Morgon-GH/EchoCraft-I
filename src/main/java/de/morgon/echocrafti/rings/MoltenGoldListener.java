8343084e3f86cb2a6713b12f50efb416f74d53d1
BuildTools <unconfigured@null.spigotmc.org>
2023-09-27 21:55:25 +0200

    removed NMS

--- src/main/java/de/morgon/echocrafti/rings/MoltenGoldListener.java
+++ src/main/java/de/morgon/echocrafti/rings/MoltenGoldListener.java
@@ -26,37 +26,37 @@ public class MoltenGoldListener implements Listener {
             if (e.getClickedBlock().getType().equals(Material.BLAST_FURNACE) && p.getItemInHand().getType().equals(Material.GOLD_INGOT)) {
 
                 Material[][][] structure = {
-                        {
-                                {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                                {Material.DEEPSLATE_TILE_WALL, Material.DEEPSLATE_TILE_WALL, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                                {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                                {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICK_STAIRS, Material.POLISHED_BLACKSTONE_BRICKS},
-                                {Material.AIR, Material.AIR, Material.AIR, Material.CHISELED_POLISHED_BLACKSTONE}
-                        }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.CHISELED_DEEPSLATE, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.AIR},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
-                }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.AIR, Material.BLAST_FURNACE, Material.DEEPSLATE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.DEEPSLATE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.CHISELED_DEEPSLATE, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
-                }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.CHISELED_DEEPSLATE, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.AIR},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
-                }, {
+                    {
                         {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
                         {Material.DEEPSLATE_TILE_WALL, Material.DEEPSLATE_TILE_WALL, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
                         {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
                         {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICK_STAIRS, Material.POLISHED_BLACKSTONE_BRICKS},
                         {Material.AIR, Material.AIR, Material.AIR, Material.CHISELED_POLISHED_BLACKSTONE}
-                }
+                    }, {
+                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
+                        {Material.CHISELED_DEEPSLATE, Material.LAVA, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.LAVA, Material.LAVA},
+                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
+                    }, {
+                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
+                        {Material.AIR, Material.BLAST_FURNACE, Material.DEEPSLATE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.DEEPSLATE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.CHISELED_DEEPSLATE, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
+                    }, {
+                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
+                        {Material.CHISELED_DEEPSLATE, Material.LAVA, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.LAVA, Material.LAVA},
+                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
+                    }, {
+                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
+                        {Material.DEEPSLATE_TILE_WALL, Material.DEEPSLATE_TILE_WALL, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICK_STAIRS, Material.POLISHED_BLACKSTONE_BRICKS},
+                        {Material.AIR, Material.AIR, Material.AIR, Material.CHISELED_POLISHED_BLACKSTONE}
+                    }
                 };
 
                 for (int x = 0; x < 5; x++) {
