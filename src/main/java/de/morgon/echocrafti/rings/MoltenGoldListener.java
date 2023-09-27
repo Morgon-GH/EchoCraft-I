8290ab680711b87105c54e6a2331717068c9f112
BuildTools <unconfigured@null.spigotmc.org>
2023-09-27 22:18:26 +0200

    testing orientation

--- src/main/java/de/morgon/echocrafti/rings/MoltenGoldListener.java
+++ src/main/java/de/morgon/echocrafti/rings/MoltenGoldListener.java
@@ -12,10 +12,12 @@ import org.bukkit.inventory.ItemStack;
 import java.awt.*;
 import java.util.Objects;
 
+import static de.morgon.echocrafti.rings.blockstructures.BlastFurnace.structure;
+
 public class MoltenGoldListener implements Listener {
 
     @EventHandler
-    public static void onClickBlastFurnace(PlayerInteractEvent e){
+    public static void onClickBlastFurnaceXZ(PlayerInteractEvent e){
 
         Player p = e.getPlayer();
 
@@ -24,40 +26,6 @@ public class MoltenGoldListener implements Listener {
         try {
 
             if (e.getClickedBlock().getType().equals(Material.BLAST_FURNACE) && p.getItemInHand().getType().equals(Material.GOLD_INGOT)) {
-
-                Material[][][] structure = {
-                    {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.DEEPSLATE_TILE_WALL, Material.DEEPSLATE_TILE_WALL, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICK_STAIRS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.CHISELED_POLISHED_BLACKSTONE}
-                    }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.CHISELED_DEEPSLATE, Material.LAVA, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.LAVA, Material.LAVA},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
-                    }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.AIR, Material.BLAST_FURNACE, Material.DEEPSLATE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.DEEPSLATE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.CHISELED_DEEPSLATE, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
-                    }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.CHISELED_DEEPSLATE, Material.LAVA, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.LAVA, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.LAVA, Material.LAVA},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS}
-                    }, {
-                        {Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN, Material.OBSIDIAN},
-                        {Material.DEEPSLATE_TILE_WALL, Material.DEEPSLATE_TILE_WALL, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICKS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.POLISHED_BLACKSTONE_BRICK_STAIRS, Material.POLISHED_BLACKSTONE_BRICKS},
-                        {Material.AIR, Material.AIR, Material.AIR, Material.CHISELED_POLISHED_BLACKSTONE}
-                    }
-                };
 
                 for (int x = 0; x < 5; x++) {
                     for (int y = 0; y < 5; y++) {
@@ -73,6 +41,113 @@ public class MoltenGoldListener implements Listener {
 
 
                 e.getPlayer().sendMessage("hi");
+                e.setCancelled(true);
+            }
+
+        }catch (Exception exception){
+            return;
+        }
+
+    }
+
+
+    @EventHandler
+    public static void onClickBlastFurnaceZX(PlayerInteractEvent e){
+
+        Player p = e.getPlayer();
+
+        ItemStack gold = new ItemStack(Material.GOLD_INGOT, 1);
+
+        try {
+
+            if (e.getClickedBlock().getType().equals(Material.BLAST_FURNACE) && p.getItemInHand().getType().equals(Material.GOLD_INGOT)) {
+
+                for (int x = 0; x < 5; x++) {
+                    for (int y = 0; y < 5; y++) {
+                        for (int z = 0; z < 4; z++) {
+
+                            if (!e.getClickedBlock().getRelative(z - 2, y - 1, x + 1).getType().equals(structure[x][y][z])) {
+                                return;
+                            }
+
+                        }
+                    }
+                }
+
+
+                e.getPlayer().sendMessage("hi");
+                e.setCancelled(true);
+            }
+
+        }catch (Exception exception){
+            return;
+        }
+
+    }
+
+
+    @EventHandler
+    public static void onClickBlastFurnace3(PlayerInteractEvent e){
+
+        Player p = e.getPlayer();
+
+        ItemStack gold = new ItemStack(Material.GOLD_INGOT, 1);
+
+        try {
+
+            if (e.getClickedBlock().getType().equals(Material.BLAST_FURNACE) && p.getItemInHand().getType().equals(Material.GOLD_INGOT)) {
+
+                for (int x = 0; x < 5; x++) {
+                    for (int y = 0; y < 5; y++) {
+                        for (int z = 0; z < 4; z++) {
+
+                            if (!e.getClickedBlock().getRelative(x - 2, y - 1, z - 1).getType().equals(structure[x][y][z])) {
+                                return;
+                            }
+
+                        }
+                    }
+                }
+
+
+                e.getPlayer().sendMessage("hi");
+                e.setCancelled(true);
+            }
+
+        }catch (Exception exception){
+            return;
+        }
+
+    }
+
+
+
+    @EventHandler
+    public static void onClickBlastFurnace4(PlayerInteractEvent e){
+
+        Player p = e.getPlayer();
+
+        ItemStack gold = new ItemStack(Material.GOLD_INGOT, 1);
+
+        try {
+
+            if (e.getClickedBlock().getType().equals(Material.BLAST_FURNACE) && p.getItemInHand().getType().equals(Material.GOLD_INGOT)) {
+
+                for (int x = 0; x < 5; x++) {
+                    for (int y = 0; y < 5; y++) {
+                        for (int z = 0; z < 4; z++) {
+
+                            if (!e.getClickedBlock().getRelative(x - 2, y - 1, z - 1).getType().equals(structure[x][y][z])) {
+                                return;
+                            }
+
+                        }
+                    }
+                }
+
+
+                e.getPlayer().sendMessage("hi");
+                e.setCancelled(true);
             }
 
         }catch (Exception exception){
